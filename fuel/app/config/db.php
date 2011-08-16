6f3bc5919e0ed975728c1ed95ebb7af6b729c509
Ben Corlett <bencorlett@me.com>
2011-08-16 21:51:33 +1000

    Changed session driver.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -1,15 +1,22 @@
 <?php
 /**
- * Fuel is a fast, lightweight, community driven PHP5 framework.
- *
+ * Spark Fuel Package By Ben Corlett
+ * 
+ * Spark - Set your fuel on fire!
+ * 
+ * The Spark Fuel Package is an open-source
+ * fuel package consisting of several 'widgets'
+ * engineered to make developing various
+ * web-based systems easier and quicker.
+ * 
  * @package    Fuel
+ * @subpackage Spark
  * @version    1.0
- * @author     Fuel Development Team
+ * @author     Ben Corlett (http://www.bencorlett.com)
  * @license    MIT License
- * @copyright  2010 - 2011 Fuel Development Team
- * @link       http://fuelphp.com
+ * @copyright  (c) 2011 Ben Corlett
+ * @link       http://spark.bencorlett.com
  */
-
 return array(
 	'active' => Config::get('environment'),
 

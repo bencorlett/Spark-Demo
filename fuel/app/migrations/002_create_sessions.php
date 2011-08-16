8bc71316487255879251b91d875bd6f27aaaf27a
Ben Corlett <bencorlett@me.com>
2011-08-16 21:55:31 +1000

    Fixed session migration.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- fuel/app/migrations/002_create_sessions.php
+++ fuel/app/migrations/002_create_sessions.php
@@ -29,6 +29,7 @@ class Create_Sessions {
 			  `session_id` varchar(40) NOT NULL,
 			  `previous_id` varchar(40) NOT NULL,
 			  `user_agent` text NOT NULL,
+			  `ip_address` char(32) NOT NULL DEFAULT '',
 			  `ip_hash` char(32) NOT NULL DEFAULT '',
 			  `created` int(10) unsigned NOT NULL DEFAULT '0',
 			  `updated` int(10) unsigned NOT NULL DEFAULT '0',
